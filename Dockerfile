0404e599e29adec334cef661db0178ad6e4a8e8a
Philipp Holler <philipp.holler93@googlemail.com>
2015-11-22 05:28:23 +0100

    tabs and comments

--- Dockerfile
+++ Dockerfile
@@ -1,36 +1,34 @@
-FROM debian:latest
+FROM debian:jessie
 MAINTAINER Michael.Bortlik@gmail.com
 
-# Definition
-	# Set environment variables for docker and entrypoint
-    ENV TEAMSPEAK_VERSION="3.0.11.4" \
-		TEAMSPEAK_SHA1="09e7fc2cb5dddf84f3356ddd555ad361f5854321" \
-		TEAMSPEAK_DATA_FOLDER="/teamspeak3" \
-		INSTALL_DIR="/opt"
+# Set environment variables for build and entrypoint
+ENV TEAMSPEAK_VERSION="3.0.11.4" \
+    TEAMSPEAK_SHA1="09e7fc2cb5dddf84f3356ddd555ad361f5854321" \
+    TEAMSPEAK_DATA_FOLDER="/teamspeak3" \
+    INSTALL_DIR="/opt"
 
-# Logic
-    # Add user and group
-    RUN groupadd -r teamspeak3 \
-     && useradd -r -g teamspeak3 teamspeak3
+# Add user and group for the service to run under
+RUN groupadd -r teamspeak3 \
+ && useradd -r -g teamspeak3 teamspeak3
 
-    # Install wget
-    RUN apt-get update \
-     && apt-get install -y wget --no-install-recommends \
-     && rm -r /var/lib/apt/lists/*
+# Install wget
+RUN apt-get update \
+ && apt-get install -y wget --no-install-recommends \
+ && rm -r /var/lib/apt/lists/*
 
-    # Download TS3 file and extract it into ${INSTALL_DIR}	
-    RUN wget -O ${INSTALL_DIR}/teamspeak3-server_linux-amd64.tar.gz http://dl.4players.de/ts/releases/${TEAMSPEAK_VERSION}/teamspeak3-server_linux-amd64-${TEAMSPEAK_VERSION}.tar.gz \
-     && echo "${TEAMSPEAK_SHA1} ${INSTALL_DIR}/teamspeak3-server_linux-amd64.tar.gz" | sha1sum -c - \
-     && tar -C ${INSTALL_DIR} -xzf ${INSTALL_DIR}/teamspeak3-server_linux-amd64.tar.gz \
-     && rm ${INSTALL_DIR}/teamspeak3-server_linux-amd64.tar.gz
+# Download TS3 file and extract it into the install directory (env)
+RUN wget -O ${INSTALL_DIR}/teamspeak3-server_linux-amd64.tar.gz http://dl.4players.de/ts/releases/${TEAMSPEAK_VERSION}/teamspeak3-server_linux-amd64-${TEAMSPEAK_VERSION}.tar.gz \
+ && echo "${TEAMSPEAK_SHA1} ${INSTALL_DIR}/teamspeak3-server_linux-amd64.tar.gz" | sha1sum -c - \
+ && tar -C ${INSTALL_DIR} -xzf ${INSTALL_DIR}/teamspeak3-server_linux-amd64.tar.gz \
+ && rm ${INSTALL_DIR}/teamspeak3-server_linux-amd64.tar.gz
 	 
-# Volume and Expose
-	VOLUME ${TEAMSPEAK_DATA_FOLDER}
-    # Expose the TS3 Ports Default: 9987, ServerQuery: 10011, File: 30033
-    EXPOSE 9987/udp 10011 30033
+# Volume for persistent data and configuration files
+VOLUME ${TEAMSPEAK_DATA_FOLDER}
+
+# Expose the TS3 Ports Default: 9987, ServerQuery: 10011, File: 30033
+EXPOSE 9987/udp 10011 30033
 	 
-# Entrypoint
-    # Add entrypoint script
-    ADD /teamspeak3_entrypoint.sh /
-    RUN chmod +x /teamspeak3_entrypoint.sh
-    ENTRYPOINT ["/teamspeak3_entrypoint.sh"]
+# Add entrypoint script and set its permissions
+ADD /teamspeak3_entrypoint.sh /
+RUN chmod +x /teamspeak3_entrypoint.sh
+ENTRYPOINT ["/teamspeak3_entrypoint.sh"]
--- teamspeak3_entrypoint.sh
+++ teamspeak3_entrypoint.sh
@@ -1,47 +1,47 @@
 #!/bin/bash
 set -e
 
-# Declare files and directories for linking
-potential_directory_links=('logs' 'files')
-potential_file_links=('query_ip_blacklist.txt' 'query_ip_whitelist.txt' 'ts3server.sqlitedb')
-potential_optional_file_links=('ts3server.ini' 'licensekey.dat' 'serverkey.dat')
+# Declare files and directories for symlinking
+directory_links=('logs' 'files')
+file_links=('query_ip_blacklist.txt' 'query_ip_whitelist.txt' 'ts3server.sqlitedb')
+optional_file_links=('ts3server.ini' 'licensekey.dat' 'serverkey.dat')
 
-# Link directoriess
-for directory in "${potential_directory_links[@]}"
+# Symlink directories
+for directory in "${directory_links[@]}"
 do
-	# Create in volume if not existing
-	if [ ! -d $TEAMSPEAK_DATA_FOLDER/$directory ]; then	
-		mkdir $TEAMSPEAK_DATA_FOLDER/$directory
-	fi
-	
-	# Recreate symlink
-	rm -rf $INSTALL_DIR/teamspeak3-server_linux-amd64/$directory
-	ln -sf $TEAMSPEAK_DATA_FOLDER/$directory $INSTALL_DIR/teamspeak3-server_linux-amd64/$directory	
+    # Create directory in volume if it doesn't exist
+    if [ ! -d $TEAMSPEAK_DATA_FOLDER/$directory ]; then	
+        mkdir $TEAMSPEAK_DATA_FOLDER/$directory
+    fi
+
+    # Recreate symlink
+    rm -rf $INSTALL_DIR/teamspeak3-server_linux-amd64/$directory
+    ln -sf $TEAMSPEAK_DATA_FOLDER/$directory $INSTALL_DIR/teamspeak3-server_linux-amd64/$directory	
 done
 
-# Link files or create and link files
-for file in "${potential_file_links[@]}"
+# Symlink files
+for file in "${file_links[@]}"
 do
-	# Create in volume if not existing
-	if [ ! -f $TEAMSPEAK_DATA_FOLDER/$file ]; then
-		touch $TEAMSPEAK_DATA_FOLDER/$file
-	fi	
-	
-	# Recreate symlink
-	rm -rf $INSTALL_DIR/teamspeak3-server_linux-amd64/$file
-	ln -sf $TEAMSPEAK_DATA_FOLDER/$file $INSTALL_DIR/teamspeak3-server_linux-amd64/$file
+    # Create file in volume if it doesn't exist
+    if [ ! -f $TEAMSPEAK_DATA_FOLDER/$file ]; then
+        touch $TEAMSPEAK_DATA_FOLDER/$file
+    fi	
+
+    # Recreate symlink
+    rm -rf $INSTALL_DIR/teamspeak3-server_linux-amd64/$file
+    ln -sf $TEAMSPEAK_DATA_FOLDER/$file $INSTALL_DIR/teamspeak3-server_linux-amd64/$file
 done
 
-# Link optional files
-for file in "${potential_optional_file_links[@]}"
+# Symlink optional files
+for file in "${optional_file_links[@]}"
 do
-	# Create symlink if file exists
-	if [ -f $TEAMSPEAK_DATA_FOLDER/$file ]; then
-		ln -sf $TEAMSPEAK_DATA_FOLDER/$file $INSTALL_DIR/teamspeak3-server_linux-amd64/$file
-	fi
+    # Create symlink to file if it exists
+    if [ -f $TEAMSPEAK_DATA_FOLDER/$file ]; then
+        ln -sf $TEAMSPEAK_DATA_FOLDER/$file $INSTALL_DIR/teamspeak3-server_linux-amd64/$file
+    fi
 done
 
-# Start Teamspeak server
+# Set correct ownership and start Teamspeak server
 chown -RL teamspeak3:teamspeak3 $INSTALL_DIR/teamspeak3-server_linux-amd64
 export LD_LIBRARY_PATH=".:$LD_LIBRARY_PATH"
 exec start-stop-daemon --start --chuid teamspeak3:teamspeak3 --chdir $INSTALL_DIR/teamspeak3-server_linux-amd64 --exec $INSTALL_DIR/teamspeak3-server_linux-amd64/ts3server_linux_amd64
