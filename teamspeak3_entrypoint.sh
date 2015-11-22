2d6aa03176d70fd5be20cbfc92062951beab36f0
Michael Bortlik <Michael.Bortlik@gmail.com>
2015-11-22 03:04:37 +0100

    fixed symlink error created an option to add license files

--- teamspeak3_entrypoint.sh
+++ teamspeak3_entrypoint.sh
@@ -3,7 +3,8 @@ set -e
 
 # Declare files and directories for linking
 potential_directory_links=('logs' 'files')
-potential_file_links=('licensekey.dat' 'query_ip_blacklist.txt' 'query_ip_whitelist.txt' 'ts3server.sqlitedb' 'ts3server.ini')
+potential_file_links=('query_ip_blacklist.txt' 'query_ip_whitelist.txt' 'ts3server.sqlitedb')
+potential_optional_file_links=('ts3server.ini' 'licensekey.dat' 'serverkey.dat')
 
 # Link directoriess
 for directory in "${potential_directory_links[@]}"
@@ -18,8 +19,8 @@ do
 	ln -sf $TEAMSPEAK_DATA_FOLDER/$directory $INSTALL_DIR/teamspeak3-server_linux-amd64/$directory	
 done
 
-# Link files
-for file in "${potential_files_links[@]}"
+# Link files or create and link files
+for file in "${potential_file_links[@]}"
 do
 	# Create in volume if not existing
 	if [ ! -f $TEAMSPEAK_DATA_FOLDER/$file ]; then
@@ -31,7 +32,16 @@ do
 	ln -sf $TEAMSPEAK_DATA_FOLDER/$file $INSTALL_DIR/teamspeak3-server_linux-amd64/$file
 done
 
+# Link optional files
+for file in "${potential_optional_file_links[@]}"
+do
+	# Create symlink if file exists
+	if [ -f $TEAMSPEAK_DATA_FOLDER/$file ]; then
+		ln -sf $TEAMSPEAK_DATA_FOLDER/$file $INSTALL_DIR/teamspeak3-server_linux-amd64/$file
+	fi
+done
+
 # Start Teamspeak server
-chown -R -H teamspeak3:teamspeak3 $INSTALL_DIR/teamspeak3-server_linux-amd64
+chown -R teamspeak3:teamspeak3 $INSTALL_DIR/teamspeak3-server_linux-amd64 $TEAMSPEAK_DATA_FOLDER
 export LD_LIBRARY_PATH=".:$LD_LIBRARY_PATH"
 exec start-stop-daemon --start --chuid teamspeak3:teamspeak3 --chdir $INSTALL_DIR/teamspeak3-server_linux-amd64 --exec $INSTALL_DIR/teamspeak3-server_linux-amd64/ts3server_linux_amd64
