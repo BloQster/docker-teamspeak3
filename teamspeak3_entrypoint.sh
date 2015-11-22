b74e2cefde18610daddf9ceec322172365fe228e
Michael Bortlik <Michael.Bortlik@gmail.com>
2015-11-22 01:02:36 +0100

    fixed non existing files

--- teamspeak3_entrypoint.sh
+++ teamspeak3_entrypoint.sh
@@ -1,28 +1,36 @@
 #!/bin/bash
 set -e
 
-
-#for linking
+# Declare files and directories for linking
 potential_directory_links=('logs' 'files')
 potential_file_links=('licensekey.dat' 'query_ip_blacklist.txt' 'query_ip_whitelist.txt' 'ts3server.sqlitedb' 'ts3server.ini')
 
-#link directories
+# Link directories
 for directory in "${potential_directory_links[@]}"
 do
-	if[ -d $directory ]; then
-		rm -rf /opt/teamspeak3-server_linux-amd64/$directory
-		ln -sf $TEAMSPEAK_DATA_FOLDER/$directory /opt/teamspeak3-server_linux-amd64/$directory
-	fi	
+	# Create in volume if not existing
+	if [ ! -d $TEAMSPEAK_DATA_FOLDER/$directory ]; then	
+		mkdir $TEAMSPEAK_DATA_FOLDER/$directory
+	fi
+	
+	# Recreate symlink
+	rm -rf $INSTALL_DIR/teamspeak3-server_linux-amd64/$directory
+	ln -sf $TEAMSPEAK_DATA_FOLDER/$directory $INSTALL_DIR/teamspeak3-server_linux-amd64/$directory	
 done
 
-#link files
+# Link files
 for file in "${potential_files_links[@]}"
 do
-	if[ -f $file ]; then
-		rm -rf /opt/teamspeak3-server_linux-amd64/$file
-		ln -sf $TEAMSPEAK_DATA_FOLDER/$file /opt/teamspeak3-server_linux-amd64/$file
+	# Create in volume if not existing
+	if [ ! -f $TEAMSPEAK_DATA_FOLDER/$file ]; then
+		touch $TEAMSPEAK_DATA_FOLDER/$file
 	fi	
+	
+	# Recreate symlink
+	rm -rf $INSTALL_DIR/teamspeak3-server_linux-amd64/$file
+	ln -sf $TEAMSPEAK_DATA_FOLDER/$file $INSTALL_DIR/teamspeak3-server_linux-amd64/$file
 done
 
 # Start Teamspeak server
-exec start-stop-daemon --start --chuid teamspeak3:teamspeak3 --exec 
+export LD_LIBRARY_PATH=".:$LD_LIBRARY_PATH"
+exec start-stop-daemon --start --chuid teamspeak3:teamspeak3 --exec $INSTALL_DIR/teamspeak3-server_linux-amd64/ts3server_linux_amd64
